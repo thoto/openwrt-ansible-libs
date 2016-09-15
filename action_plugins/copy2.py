1769814a687f6599a687e5fc93b46705e296b0fb
Thorsten M <thoto@thoto.net>
2016-09-15 23:53:10 +0200

    added copy2 action to switch module in copy action

---
+++ action_plugins/copy2.py
@@ -0,0 +1,38 @@
+from ansible.plugins.action import ActionBase
+
+class ActionModule(ActionBase):
+
+    def __init__(self, task, connection, play_context, loader, templar,\
+            shared_loader_obj):
+        super(ActionModule, self).__init__(task, connection, play_context,\
+                loader, templar, shared_loader_obj)
+
+        self._copyplugin=shared_loader_obj.action_loader.get("copy",\
+                task, connection, play_context, loader, templar,\
+                shared_loader_obj)
+
+
+    def run(self, tmp=None, task_vars=None, **kwargs):
+        ''' runs standard copy module or custom copy.lua/copy.sh if 
+        ansible_distribution is set OpenWRT '''
+        if task_vars is None:
+            task_vars = dict()
+
+        result = super(ActionModule, self).run(tmp, task_vars)
+
+        if "ansible_distribution" in task_vars \
+                and task_vars["ansible_distribution"]=='OpenWRT':
+            old_pref=self._connection.module_implementation_preferences
+            self._connection.module_implementation_preferences=(".lua",".sh","")
+
+        self._copyplugin=self._shared_loader_obj.action_loader.get("copy",\
+                self._task, self._connection, self._play_context,\
+                self._loader, self._templar, self._shared_loader_obj)
+        
+        result.update(self._copyplugin.run(tmp,task_vars))
+
+        if "ansible_distribution" in task_vars \
+                and task_vars["ansible_distribution"]=='OpenWRT':
+            self._connection.module_implementation_preferences=old_pref
+
+        return result
